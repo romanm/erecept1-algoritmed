9e63821396a783dc32edaad2fa121b879c5d91c0
romanm <roman.mishchenko@gmail.com>
2019-03-06 20:28:33 +0200

    eRecept

--- src/main/resources/static/f/js/app001.js
+++ src/main/resources/static/f/js/app001.js
@@ -2,4 +2,84 @@ var app = angular.module('myApp', ['ngSanitize']);
 var exe_fn = {}
 var initApp = function($scope, $http){
 	console.log(123)
+	exe_fn = new Exe_fn($scope, $http);
+	exe_fn.httpGet_j2c_table_db1_params_then_fn = function(params, then_fn){
+		return {
+			url : '/r/url_sql_read_db1',
+			params : params,
+			then_fn : then_fn,
+	}	}
+	$scope.elementsMap = {}
+	$scope.referencesMap = {}
 }
+
+function replaceParams(params){
+//	console.log(params.sql)
+	angular.forEach(params.sql.split(':'), function(v,k){
+		if(k>0){
+			var p = v.split(' ')[0].replace(')','').replace(',','').replace(';','').trim()
+			var pv = params[p]
+//			console.log(p+' = '+pv)
+			if(pv){
+				params.sql = params.sql.replace(':'+p, "'"+pv+"'")
+			}
+		}
+	})
+//	console.log(params.ts_value)
+//	console.log(params)
+//	console.log(params.sql)
+}
+
+function readSql(params, obj){
+	replaceParams(params)
+	if(!obj) obj = params
+	exe_fn.httpGet(exe_fn.httpGet_j2c_table_db1_params_then_fn(
+	params,
+	function(response) {
+		obj.list = response.data.list
+		if(obj.afterRead){
+			obj.afterRead(response)
+		}else if(params.afterRead){
+			params.afterRead(response)
+		}
+	}))
+}
+
+
+function Exe_fn($scope, $http){
+	this.httpGet=function(progr_am){
+		if(progr_am.error_fn)
+			$http
+			.get(progr_am.url, {params:progr_am.params})
+			.then(progr_am.then_fn, progr_am.error_fn)
+		else
+			$http
+			.get(progr_am.url, {params:progr_am.params})
+			.then(progr_am.then_fn)
+	}
+	this.httpPost=function(progr_am){
+		if(progr_am.error_fn)
+			$http.post(progr_am.url, progr_am.data)
+			.then(progr_am.then_fn, progr_am.error_fn)
+		else
+			$http.post(progr_am.url, progr_am.data)
+			.then(progr_am.then_fn)
+	}
+}
+
+function json_elementsMap(json, elementsMap, referencesMap){
+	elementsMap[json.doc_id] = json
+	if(json.reference){
+		referencesMap[json.reference] = null
+	}
+	angular.forEach(json.children, function(v){
+		json_elementsMap(v, elementsMap, referencesMap)
+	})
+}
+
+var sql_app = {}
+sql_app.amk025_template = function(){
+	return "SELECT * FROM doc d2, doc d1,docbody " +
+	"WHERE d1.doc_id=docbody_id AND d2.doc_id=d1.parent AND d2.doctype IN (6,17) AND d1.reference=:jsonId"
+}
+
--- src/main/resources/static/f/recept/start1.js
+++ src/main/resources/static/f/recept/start1.js
@@ -1,3 +1,33 @@
 app.controller('myCtrl', function($scope, $http, $interval, $filter) {
-	console.log(123)
+	initApp($scope, $http)
+
+	console.log(exe_fn)
+	
+	exe_fn.httpGet({url:'/f/json/getMedicalRequestById.json',
+		then_fn:function(response){
+			$scope.recept_sample = response.data.data
+			console.log($scope.recept_sample)
+		}
+	})
+
+	exe_fn.httpGet({url:'/f/json/dictionaries.json',
+		then_fn:function(response){
+			$scope.dictionaries = response.data.data
+			console.log($scope.dictionaries)
+		}
+	})
+
+	readSql({
+		sql:sql_app.amk025_template(),
+		jsonId:87026,
+		afterRead:function(response){
+			$scope.recept_template = JSON.parse(response.data.list[0].docbody)
+			json_elementsMap($scope.recept_template.docRoot, $scope.elementsMap, $scope.referencesMap)
+			console.log($scope.recept_template, $scope.elementsMap, $scope.referencesMap)
+			/*
+			readRef($scope)
+			 */
+		}
+	})
+
 })
