1291b12b60d0eda977195263d4e75df1e4473efc
romanm <roman.mishchenko@gmail.com>
2019-03-28 01:04:26 +0200

    editor of document

--- src/main/resources/static/f/new_apteka/start1.js
+++ src/main/resources/static/f/new_apteka/start1.js
@@ -23,6 +23,7 @@ app.controller('AppCtrl', function($scope, $http, $interval, $filter) {
 	
 	var set_legal_entitie = function(o){
 		ctrl.legal_entitie = o
+		console.log('ctrl.legal_entitie',ctrl.legal_entitie)
 		$scope.elementsMap[o.doc_id] = o
 		$scope.elementsMap[o.reference].value_element = o
 //		console.log(ctrl.legal_entitie.doc_id, ctrl.legal_entitie,1111111, $scope.elementsMap[o.reference], o.reference, o.doc_id)
@@ -34,8 +35,18 @@ app.controller('AppCtrl', function($scope, $http, $interval, $filter) {
 				angular.forEach(ctrl.legal_entitie.children, function(v){
 					mapElement(v, $scope.elementsMap)
 //					console.log(v, $scope.elementsMap[v.reference])
-					if($scope.elementsMap[v.reference]){
-						$scope.elementsMap[v.reference].value_element = v
+					var legal_entitie_template_element = $scope.elementsMap[v.reference]
+					if(legal_entitie_template_element){
+						if(legal_entitie_template_element.doctype>=32||legal_entitie_template_element.doctype<=37){
+							// тип [] - масив елементів поєднання один до багатьох
+							if(!$scope.elementsMap[v.reference].value_elements){
+								$scope.elementsMap[v.reference].value_elements = []
+							}
+							$scope.elementsMap[v.reference].value_elements.push(v)
+						}else{
+							// тип {} - елемент поєднання один до одного
+							$scope.elementsMap[v.reference].value_element = v
+						}
 					}
 				})
 			},
@@ -61,7 +72,12 @@ app.controller('AppCtrl', function($scope, $http, $interval, $filter) {
 		afterRead:function(response){
 			$scope.eHealth_template = JSON.parse(response.data.list[0].docbody)
 			$scope.legal_entitie_template = $scope.eHealth_template.docRoot.children[2].children[0]
-			mapElement($scope.legal_entitie_template, $scope.elementsMap)
+			mapElement($scope.eHealth_template.docRoot, $scope.elementsMap)
+			console.log(115789
+					, $scope.elementsMap[115789]
+			,$scope.elementsMap[115789].reference
+			, $scope.elementsMap[$scope.elementsMap[115789].reference]
+			)
 			if(false)
 			console.log(
 				$scope.legal_entitie_template,
@@ -85,65 +101,100 @@ app.controller('AppCtrl', function($scope, $http, $interval, $filter) {
 		return r
 	}
 	$scope.editDoc = {}
+	ctrl.editDoc = $scope.editDoc
 	
+	ctrl.editDoc.editForm = {}
+	ctrl.elementsMap = $scope.elementsMap
+	ctrl.editDoc.addList = function(o){
+		ctrl.editDoc.focus(o)
+		console.log('ctrl.editDoc.addList',o,ctrl.elementsMap[o.parent])
+		o.value_element.reference2 = o.reference
+		var sql = insertDocElement1(o) + sql_update_reference2(o)
+		writeDocElement1(o,sql)
+	}
+	
+	ctrl.editDoc.focus = function(o){
+		if(!o.value_element){
+			o.value_element = {reference:o.doc_id, parent:ctrl.elementsMap[o.parent].value_element.doc_id}
+		}
+		console.log('focus',o.doc_id,o.reference,o.value,!o.value_element,o, $scope.elementsMap[o.parent],o.value_element)
+		if(o.reference && !$scope.elementsMap[o.reference]){
+			readSelectData(o.reference, o.string_reference)
+		}
+	}
+
+	var insertDocElement1 = function(o){
+		return  "INSERT INTO doc (doc_id,parent,reference,doctype) " +
+		"VALUES (:nextDbId1, " + o.value_element.parent + ", " + o.value_element.reference + ", 18);\n"
+	}
+	var sql_update_reference2 = function(o){
+		return "UPDATE doc SET reference2 = " + o.value_element.reference2 + " WHERE doc_id=:nextDbId1;"
+	}
+
+	$scope.editDoc.blur_select = function(o){
+		console.log('blur_select',o)
+		if(o.value_element.doc_id){
+			var sql = sql_update_reference2(o).replace(':nextDbId1',o.value_element.doc_id)
+			writeDocElement1(o,sql)
+		}else{
+			var sql = insertDocElement1(o) + sql_update_reference2(o)
+			writeDocElement1(o,sql)
+		}
+	}
+
 	$scope.editDoc.blur = function(o){
 		console.log('blur', o.doc_id,o.value,'\n value_element = ',o.value_element)
 		if(o.value_element.doc_id){
 			var sql = "UPDATE string SET value = '" + o.value_element.value + "' WHERE string_id=" + o.value_element.doc_id + ";"
 //			console.log(sql)
-			writeSql({
-				sql:sql,
-				dataAfterSave:function(response){
-					console.log(response.data)
-				}
-			})
+			writeDocElement1(o,sql)
 		}else{
 			if(o.value_element.value){
-				var sql = "INSERT INTO doc (doc_id,parent,reference,doctype) " +
-				"VALUES (:nextDbId1, " + o.value_element.parent + "," + o.value_element.reference + ", 18);\n" +
+				var sql = insertDocElement1(o) +
 				"INSERT INTO string (string_id,value) " +
 				"VALUES (:nextDbId1, '" + o.value_element.value + "');"
 //				console.log(sql)
-				writeSql({
-					sql:sql,
-					dataAfterSave:function(response){
-						o.value_element.doc_id = response.data.nextDbId1
-//						console.log(response.data, o.value_element)
-					}
-				})
+				writeDocElement1(o,sql)
 			}
 		}
 	}
-	$scope.editDoc.blur_select = function(o){
-		console.log('blur_select',o)
-	}
-	$scope.editDoc.focus = function(o){
-		if(!o.value_element){
-			o.value_element = {reference:o.doc_id,parent:$scope.elementsMap[o.parent].value_element.doc_id}
-		}
-		console.log('focus',o.doc_id,o.value,!o.value_element,o, $scope.elementsMap[o.parent],o.value_element)
-		if(o.reference){
-			var sql = "SELECT d1.*, s1.value, s2.value value_ua " +
-			"FROM doc d1, string s1, doc d2, string s2 " +
-			"WHERE d1.parent=" + o.reference +" " +
-			"AND d2.reference=d1.doc_id " +
-			"AND s1.string_id=d1.doc_id " +
-			"AND s2.string_id=d2.doc_id " +
-			"AND d2.parent=115924; "
-			readSql({
-				sql:sql,
-				afterRead:function(response){
-					var o2 = {doc_id:o.reference, value:o.string_reference}
-					o2.children = response.data.list
-					$scope.elementsMap[o.reference] = o2
-					console.log(o2)
+
+	var writeDocElement1 = function(o,sql){
+		writeSql({
+			sql:sql,
+			dataAfterSave:function(response){
+				if(response.data.nextDbId1){
+					o.value_element.doc_id = response.data.nextDbId1
 				}
-			})
-		}
+				console.log('writeDocElement1 ',response.data, o.value_element)
+			}
+		})
 	}
-	$scope.editDoc.change = function(o){
-		console.log("change",o.doc_id)
+
+	var readSelectData = function(reference, string_reference){
+		var sql = "SELECT d1.*, s1.value, s2.value value_ua " +
+		"FROM doc d1, string s1, doc d2, string s2 \n" +
+		"WHERE d1.parent=" + reference +" " +
+		"AND d2.reference=d1.doc_id " +
+		"AND s1.string_id=d1.doc_id " +
+		"AND s2.string_id=d2.doc_id " +
+		"AND d2.parent=115924; "
+		readSql({
+			sql:sql,
+			afterRead:function(response){
+				var o2 = {doc_id:reference, value:string_reference}
+				o2.children = response.data.list
+				$scope.elementsMap[reference] = o2
+//				console.log(reference, string_reference, o2, sql)
+			}
+		})
 	}
+
+	readSelectData(117239, 'LEGAL_ENTITY_TYPE')
+	readSelectData(117018, 'OWNER_PROPERTY_TYPE')
+	readSelectData(117301, 'LEGAL_FORM')
+	
+	
 })
 
 var field_names = {
