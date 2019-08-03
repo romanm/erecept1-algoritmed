79882aafc8a0999ab69346da2adc1e9dab224240
romanm <roman.mishchenko@gmail.com>
2019-08-03 23:43:47 +0300

    protocol2icpc2

--- src/main/resources/static/f/dev3/icpc2duodecim.js
+++ src/main/resources/static/f/dev3/icpc2duodecim.js
@@ -6,5 +6,37 @@ app.controller('AppCtrl', function($scope, $http) {
 	.then(function(r){
 		ctrl.icp2duodecim = r.data
 		console.log(ctrl.icp2duodecim)
+		calcProtocolNumbers(ctrl)
 	})
+	ctrl.clicked = {}
+	ctrl.clicICPC2 = function(protocol, icpc2){
+		ctrl.clicked.protocol = protocol
+		ctrl.clicked.icpc2 = icpc2
+		console.log(protocol, icpc2)
+	}
 })
+
+var otherICPC2 = function(ctrl, protocol, icpc2){
+	if(!ctrl.embNrs[protocol])
+		ctrl.embNrs[protocol] = []
+	if(!ctrl.embNrs[protocol].includes(icpc2))
+	ctrl.embNrs[protocol].push(icpc2)
+}
+
+var calcProtocolNumbers = function(ctrl){
+	console.log(123)
+	var i = 0 
+	ctrl.embNrs = {}
+	angular.forEach(ctrl.icp2duodecim, function(v,icpc2){
+		
+		angular.forEach(v.primary, function(protocol){
+			i++
+			otherICPC2(ctrl, protocol, icpc2)
+		})
+		angular.forEach(v.secondary, function(protocol){
+			i++
+			otherICPC2(ctrl, protocol, icpc2)
+		})
+	})
+	console.log(i, ctrl.embNrs)
+}
