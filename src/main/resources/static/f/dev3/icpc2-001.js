bcbfdcb7fea902d22e579b3b4398c9070a85fab7
romanm <roman.mishchenko@gmail.com>
2019-08-06 01:15:14 +0300

    ins group icpc2

--- src/main/resources/static/f/dev3/icpc2-001.js
+++ src/main/resources/static/f/dev3/icpc2-001.js
@@ -1,11 +1,41 @@
 app.controller('AppCtrl', function($scope, $http) {
 	var ctrl = this
 	ctrl.page_title = 'icp2-001'
+	initApp($scope, $http, ctrl)
+
 	$http
 	.get("/f/dev3/ICPC2-ua.json")
 	.then(function(r){
 		ctrl.icpc2 = r.data
-		console.log(ctrl.icpc2)
+//		insertICPC2_GROUP(ctrl)
 	})
 })
 
+var insertICPC2_GROUP = function(ctrl){
+//	console.log(ctrl.icpc2.group)
+	var sqls = ""
+	angular.forEach(ctrl.icpc2.group, function(v,k){
+		var sql = "" +
+		"INSERT INTO doc (doc_id,parent,doctype) " +
+		"VALUES (:nextDbId1, 285598, 18);\n" +
+		"INSERT INTO string (string_id, value) VALUES (:nextDbId1, '" +
+		k +
+		"');\n" +
+		"INSERT INTO doc (doc_id,parent,doctype,reference) " +
+		"VALUES (:nextDbId2,  285597, 18, :nextDbId1 );\n" +
+		"INSERT INTO string (string_id, value) VALUES (:nextDbId2, '" +
+		v.name.replace(/'/g,"''") +
+		"');" +
+		""
+		if(true||'A'==k){
+			console.log(k,v)
+			console.log( sql)
+			writeSql({sql : sql,
+			dataAfterSave:function(response){
+				console.log(response.data)
+			}
+		})
+		}
+	})
+	console.log(sqls)
+}
