28b16577d142e18a92cd4c2c10de8efc52285ba9
romanm <roman.mishchenko@gmail.com>
2019-08-09 03:34:29 +0300

    readWriteICD10_goroch2

--- src/main/resources/static/f/dev3/icd10-001.js
+++ src/main/resources/static/f/dev3/icd10-001.js
@@ -33,7 +33,7 @@ app.controller('AppCtrl', function($scope, $http, $timeout) {
 	"AND d1.doc_id=d2.reference \n" +
 	""
 //	+ "ORDER BY treelevel, sort "
-	console.log(sql_seek)
+//	console.log(sql_seek)
 	
 	var rootIcd10Id = 287136
 	ctrl.icd10 = {doc_id:rootIcd10Id, openChildren:true}
@@ -47,8 +47,66 @@ app.controller('AppCtrl', function($scope, $http, $timeout) {
 //	readWriteICD10_Chapter(ctrl)
 //	readWriteICD10_l4(ctrl)
 //	readWriteICD10_l5(ctrl)
+//	readWriteICD10_goroch1(ctrl)
+//	readWriteICD10_goroch2(ctrl)
 })
 
+/*
+SELECT * FROM icd b
+left join (SELECT * FROM doc,string where string_id=reference and parent=287138) a on b.icd_code=a.value
+where doc_id is null
+order by icd_code
+ * */
+
+var parentI18nId = 287138 //UA_i18n in ICD10 DataDictionary
+var readWriteICD10_goroch2 = function(ctrl){
+	var sql2 = "SELECT a.*, c.sort_id, treelevel FROM ( \n" +
+	"SELECT substring(icd_10,0,4) icd1, i.* FROM icpc2_icd10 i \n" +
+	"left join (SELECT * FROM doc,string where string_id=reference and parent=287138) x on value=icd_10 \n" +
+	"where value is null \n" +
+	") a \n" +
+	"left join (SELECT * FROM doc,string, sort where sort_id=reference and string_id=reference and parent=287138) c on value=icd1 \n" +
+	"where sort_id is not null \n" +
+	"order by icd1, icd_10"
+	console.log(sql2)
+	readSql({ sql:sql2, afterRead:function(r){
+		console.log(r.data)
+		angular.forEach(r.data.list, function(v, sort){
+			if(true||1>sort){
+				var sql = sql_app.insert_CODE_i18n_sort(v.sort_id, v.icd_10, parentI18nId, v.n______icd, sort, v.treelevel+1)
+				console.log(sort,v, sql)
+				writeSql({sql : sql, dataAfterSave:function(response){
+					console.log(response.data)
+				}})
+			}
+		})
+	}})
+}
+var readWriteICD10_goroch1 = function(ctrl){
+	var sql2 = "SELECT a.*, c.sort_id, treelevel FROM ( \n" +
+	"SELECT substring(icd_10,0,4) icd1, i.* FROM icpc2_icd10 i \n" +
+	"left join (SELECT * FROM doc,string where string_id=reference and parent=287138) x on value=icd_10 \n" +
+	"where value is null \n" +
+	") a \n" +
+	"left join (SELECT * FROM icd) b on icd_code=icd_10 \n" +
+	"left join (SELECT * FROM doc,string, sort where sort_id=reference and string_id=reference and parent=287138) c on value=icd1 \n" +
+	"where icd_id is not null \n" +
+	"order by icd1, icd_10"
+	console.log(sql2)
+	readSql({ sql:sql2, afterRead:function(r){
+		console.log(r.data)
+		angular.forEach(r.data.list, function(v, sort){
+			if(true||1>sort){
+				var sql = sql_app.insert_CODE_i18n_sort(v.sort_id, v.icd_10, parentI18nId, v.n______icd, sort, v.treelevel+1)
+				console.log(sort,v, sql)
+				writeSql({sql : sql, dataAfterSave:function(response){
+					console.log(response.data)
+				}})
+			}
+		})
+	}})
+
+}
 var readWriteICD10_l5 = function(ctrl){
 	var sl = "SELECT s.*, icd_id p_id, t.* FROM doc, sort t,string s, icd " +
 	"WHERE doc_id=string_id AND value=icd_code and doc_id=sort_id and treelevel=4"
@@ -132,8 +190,6 @@ var readWriteICD10_l2 = function(ctrl){
 	}})
 	
 }
-
-var parentI18nId = 287138 //UA_i18n in ICD10 DataDictionary 
 var readICD10 = function(ctrl, parentId) {
 	var sql = "SELECT d1.doc_id, d1.parent, sort, treelevel, s1.value code, s2.value i18n \n" +
 	"FROM doc d1, sort, string s1, doc d2, string s2 \n" +
