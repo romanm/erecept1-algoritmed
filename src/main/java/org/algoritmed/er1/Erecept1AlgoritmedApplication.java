529ebf95095569c0517a2b64c82433cc26748a76
romanm <roman.mishchenko@gmail.com>
2019-12-11 00:05:12 +0200

    1.7.9 angularjs

--- src/main/java/org/algoritmed/er1/Erecept1AlgoritmedApplication.java
+++ src/main/java/org/algoritmed/er1/Erecept1AlgoritmedApplication.java
@@ -1,5 +1,7 @@
 package org.algoritmed.er1;
 
+import java.time.Instant;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -10,6 +12,10 @@ public class Erecept1AlgoritmedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Erecept1AlgoritmedApplication.class, args);
+		Instant now     = Instant.now();
+		System.out.println("------16--------Erecept1AlgoritmedApplication--------------"
+				+ "\n"
+				+ "------"+now);
 	}
 
 }
