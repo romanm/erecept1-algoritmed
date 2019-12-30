15adc5e6fdb464ecf804f9c4c965611fb5ace6d7
romanm <roman.mishchenko@gmail.com>
2019-12-31 00:13:39 +0200

    ordnung

--- src/main/resources/static/f/dev7/achi.js
+++ src/main/resources/static/f/dev7/achi.js
@@ -2,31 +2,37 @@ var app = angular.module('myApp', ['ngSanitize']);
 app.controller('AppCtrl', function($scope, $http, $timeout) {
 	var ctrl = this
 	ctrl.page_title = 'ACHI'
+	ctrl.page_title = 'АКМІ'
 	initApp($scope, $http, ctrl, $timeout)
 	initAchi(ctrl)
 	console.log('--',ctrl.page_title,'--')
 //	read2(ctrl)
 	read_dataObject(ctrl, 'seek_achi', ctrl.seek_sql) 
-	read_dataObject(ctrl, 'l1', ctrl.l1_sql)
-	read_dataObject(ctrl, 'l2', ctrl.l2_sql, 122, true)
+	read_dataObject(ctrl, 'l0', ctrl.l0_sql)
+	read_dataObject(ctrl, 'l1', ctrl.l1_sql, 122, true)
 })
 
 function initAchi(ctrl) {
-	ctrl.l1_fn = {}
-	ctrl.l1_fn.filters = {}
+	ctrl.l0_fn = {}
+	ctrl.l0_fn.filters = {}
 	ctrl.achi_seek_head = {
 		n9:{n:'Код', style:{'width':'100px'}},
 		n8:{n:'ACHI'},
 	}
 	ctrl.l2_head = {
 		cnt:{n:'∑', style:{'width':'35px'}},
-		l1:{n:'№', style:{'width':'55px'}},
-		n3:{n:'№', style:{'width':'25px'}},
-		n4:{n:'Топологія'},
+		n5:{n:'№', style:{'width':'25px'}},
+		n6:{n:'Вісь процедурної типології'},
 	}
 	ctrl.l1_head = {
 		cnt:{n:'∑', style:{'width':'35px'}},
-		l1:{n:'№', style:{'width':'55px'}},
+		l0:{n:'№', style:{'width':'55px'}},
+		n3:{n:'№', style:{'width':'25px'}},
+		n4:{n:'Вісь анатомічної локалізації'},
+	}
+	ctrl.l0_head = {
+		cnt:{n:'∑', style:{'width':'35px'}},
+		l0:{n:'№', style:{'width':'55px'}},
 		n2:{n:'Клас'},
 	}
 
@@ -38,67 +44,67 @@ function initAchi(ctrl) {
 	}
 
 	ctrl.seek_sql = "SELECT * FROM achi_ukr_eng2_3"
-//ctrl.l1_sql = "SELECT l0_id , count(*) cnt, min(l1) l1, min(n2) n2 FROM achi_ukr_eng2_3 group by l0_id"
-	ctrl.l1_sql = "SELECT *, SPLIT_PART(l1s, ' ',2)::INT l1 FROM " +
-		"(SELECT l1_id , COUNT(*) cnt, MIN(n1) l1s, MIN(n2) n2 FROM achi_ukr_eng2_3 GROUP BY l1_id) a "
-	ctrl.l2_sql = "SELECT *, SPLIT_PART(l1s, ' ',2)::INT l1 FROM ( " +
-		"SELECT l2_id, COUNT(*) cnt, MIN(n1) l1s, MIN(n3) n3, MIN(n4) n4, MIN(l1_id) l1_id FROM achi_ukr_eng2_3 GROUP BY l2_id" +
+//ctrl.l0_sql = "SELECT l0_id , count(*) cnt, min(l0) l0, min(n2) n2 FROM achi_ukr_eng2_3 group by l0_id"
+	ctrl.l0_sql = "SELECT *, SPLIT_PART(l0s, ' ',2)::INT l0 FROM " +
+		"(SELECT l0_id , COUNT(*) cnt, MIN(n1) l0s, MIN(n2) n2 FROM achi_ukr_eng2_3 GROUP BY l0_id) a "
+	ctrl.l1_sql = "SELECT *, SPLIT_PART(l0s, ' ',2)::INT l0 FROM ( " +
+		"SELECT l1_id, COUNT(*) cnt, MIN(n1) l0s, MIN(n3) n3, MIN(n4) n4, MIN(l0_id) l0_id FROM achi_ukr_eng2_3 GROUP BY l1_id" +
 		") a"
-//	console.log(ctrl.l2_sql)
-	initL1(ctrl)
+//	console.log(ctrl.l1_sql)
+	initl0(ctrl)
 }
 
-function initL1(ctrl) {
-	ctrl.l1_fn.click_head = function(h){
-		if(!ctrl.l1_order.includes(h)){
-			ctrl.l1_order = h
+function initl0(ctrl) {
+	ctrl.l0_fn.click_head = function(h){
+		if(!ctrl.l0_order.includes(h)){
+			ctrl.l0_order = h
 		}else{
-			if(!ctrl.l1_order.includes('DESC')){
-				ctrl.l1_order += ' DESC'
+			if(!ctrl.l0_order.includes('DESC')){
+				ctrl.l0_order += ' DESC'
 			}else{
-				ctrl.l1_order = h
+				ctrl.l0_order = h
 			}
 		}
-		var sql = "SELECT * FROM (" + ctrl.l1_sql + ") a ORDER BY "+ctrl.l1_order
-//	console.log(h, ctrl.l1_order.includes(h), sql)
-		read_dataObject(ctrl, 'l1', sql) 
+		var sql = "SELECT * FROM (" + ctrl.l0_sql + ") a ORDER BY "+ctrl.l0_order
+//	console.log(h, ctrl.l0_order.includes(h), sql)
+		read_dataObject(ctrl, 'l0', sql) 
 	}
-	ctrl.l1_fn.remove_filter = function(v){
+	ctrl.l0_fn.remove_filter = function(v){
 		delete ctrl[v+'_fn'].filters[v]
 	}
 
-	ctrl.l1_fn.click_row = function(l1){
-		if(ctrl.l1_fn.filters.l1 && l1.l1_id == ctrl.l1_fn.filters.l1.l1_id){
-			delete ctrl.l1_fn.filters.l1
+	ctrl.l0_fn.click_row = function(l0){
+		if(ctrl.l0_fn.filters.l0 && l0.l0_id == ctrl.l0_fn.filters.l0.l0_id){
+			delete ctrl.l0_fn.filters.l0
 			return
 		}
-		ctrl.l1_fn.filters.l1 = l1
-		console.log(ctrl.l1_fn, l1)
+		ctrl.l0_fn.filters.l0 = l0
+		console.log(ctrl.l0_fn, l0)
 // seek ACHI with filter
 		console.log(ctrl.seekLogic.seek_value)
-		var sql = ctrl.seek_sql + " WHERE l1_id = " + l1.l1_id
+		var sql = ctrl.seek_sql + " WHERE l0_id = " + l0.l0_id
 		console.log(sql)
 		read_dataObject(ctrl, 'seek_achi', sql)
 	}
 
-	ctrl.l1_order = ""
+	ctrl.l0_order = ""
 }
 
 
 var sql_read1 = "SELECT * FROM (" +
-"SELECT l0_id , COUNT(*) cnt, MIN(n1) l1 FROM achi_ukr_eng2_3 " +
+"SELECT l0_id , COUNT(*) cnt, MIN(n1) l0 FROM achi_ukr_eng2_3 " +
 "WHERE l0_id IS NOT NULL GROUP BY l0_id ) a " +
 "WHERE cnt=1"
 
-var sql_read2 = "SELECT * FROM (SELECT l2_id , COUNT(*) cnt, MIN(n4) l2, min(l1_id) l1_id " +
-	" FROM achi_ukr_eng2_3 WHERE l2_id IS NOT NULL GROUP BY l2_id ) a " +
+var sql_read2 = "SELECT * FROM (SELECT l1_id , COUNT(*) cnt, MIN(n4) l1, min(l0_id) l0_id " +
+	" FROM achi_ukr_eng2_3 WHERE l1_id IS NOT NULL GROUP BY l1_id ) a " +
 	" WHERE cnt=1 "
 //console.log(sql_read2)
 
 function write2_1(ctrl,v){
-	v.l2_1 = v.l2.replace(/'/g,"''")
+	v.l1_1 = v.l1.replace(/'/g,"''")
 	console.log(v)
-	v.sql = "UPDATE achi_ukr_eng2_3 SET l2_id=:l2_id WHERE n4=:l2_1"
+	v.sql = "UPDATE achi_ukr_eng2_3 SET l1_id=:l1_id WHERE n4=:l1_1"
 	v.dataAfterSave = function(response){
 		console.log(response.data, v.sql)
 	}
@@ -136,7 +142,7 @@ function read1(ctrl) {
 
 function read1_2(ctrl,v){
 	console.log(v)
-	v.sql = "UPDATE achi_ukr_eng2_3 SET l0_id=:l0_id WHERE l1=:l1"
+	v.sql = "UPDATE achi_ukr_eng2_3 SET l0_id=:l0_id WHERE l0=:l0"
 	v.dataAfterSave = function(response){
 		console.log(response.data, v.sql)
 	}
