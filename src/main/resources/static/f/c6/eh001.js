cd1d28a0932c8ee5ebb01006ebee306b1be0537e
romanm <roman.mishchenko@gmail.com>
2020-01-05 01:33:09 +0200

    ctrl.doc_data_shortView

--- src/main/resources/static/f/c6/eh001.js
+++ src/main/resources/static/f/c6/eh001.js
@@ -1,7 +1,8 @@
 var initEh001 = function() {
 	ctrl.click_data_row = function(d){if(!d.children){
 		console.log(d)
-		read_dataObject2(sql_app.obj_with_parent(d.doc_id), function(response){if(response.data.list.length>0){
+		read_dataObject2(sql_app.obj_with_parent(d.doc_id)
+				, function(response){ if(response.data.list.length>0){
 			d.children = response.data.list
 			d.cols = {}
 			angular.forEach(d.children, function(v){
@@ -14,13 +15,34 @@ var initEh001 = function() {
 			ctrl.doc_rows = response.data.list
 		})
 	}
+	ctrl.doc_data_shortView = {}
+	ctrl.doc_data_shortView._115827 = [115783]
+	ctrl.doc_data_shortView._115856 = [115879]
 	sql_app.obj_with_reference = function(reference){
-		var sql = "SELECT * FROM doc where :reference in (reference)"
+		var sql = "" +
+		"SELECT d.* FROM doc d " +
+		"WHERE :reference IN (d.reference) "
 		sql = sql.replace(':reference', reference)
+		console.log(sql, reference, ctrl.doc_data_shortView)
+		var sv = ctrl.doc_data_shortView['_'+reference]
+		if(sv){
+			var lf_sqls=' doc d \n', lf_cols=' d.* '
+			angular.forEach(sv, function(v,k){
+//				console.log(v, k)
+				lf_cols += ", s" +k+".value s_"+v+"_value "
+				lf_sqls += "" +
+				"LEFT JOIN doc d" + k + 
+				" LEFT JOIN string s" + k + " ON s" + k + ".string_id=d" + k + ".doc_id " +
+				" ON d" + k + ".parent = d.doc_id AND d" + k + ".reference = "+v +"\n"
+			})
+//			console.log(lf_sqls, lf_cols)
+			sql = sql.replace(' doc d ', lf_sqls)
+			sql = sql.replace(' d.* ', lf_cols)
+			console.log(sql)
+		}
 		return sql
 	}
 	ctrl.children_close = function(d){
-		console.log(d)
 		if(d.children){
 			d.children_close = !d.children_close
 		}
@@ -37,9 +59,9 @@ var initEh001 = function() {
 	sql_app.obj_with_parent= function(parent){
 		var sql = "\n" +
 		"SELECT d1.*, sort, s1.value s1value FROM doc d1 \n" +
-		"LEFT JOIN string s1 ON d1.doc_id= s1.string_id \n" +
-		"LEFT JOIN string s2 ON d1.reference= s2.string_id \n" +
-		"LEFT JOIN sort o1 ON o1.sort_id=d1.doc_id \n" +
+		"LEFT JOIN string s1 ON d1.doc_id = s1.string_id \n" +
+		"LEFT JOIN string s2 ON d1.reference = s2.string_id \n" +
+		"LEFT JOIN sort o1 ON o1.sort_id = d1.doc_id \n" +
 		"WHERE d1.parent=:parent \n" +
 		"ORDER BY sort "
 		sql = sql.replace(':parent', parent)
@@ -61,6 +83,7 @@ app.controller('AppCtrl', function($scope, $http, $timeout) {
 	ctrl.page_title = 'eh001'
 	initApp($scope, $http, $timeout)
 	initEh001()
-	read_dataObject('docs', sql_app.obj_with_parent(115800), null, true)
+	read_dataObject('docs', sql_app.obj_with_parent(115800))
 	ctrl.set_choice_doc({doc_id: 115827, s1value:'legal_entitie'})
 })
+
